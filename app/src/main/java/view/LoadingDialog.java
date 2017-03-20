20e171d6869fa0740c78cefd19c579ef22160312
17828127004 <172828913@qq.com>
2017-03-20 15:31:56 +0800

    Initial commit

---
+++ app/src/main/java/view/LoadingDialog.java
@@ -0,0 +1,40 @@
+package view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.kxhl.R;
+
+
+/**
+ * Created by Administrator on 2017/3/1.
+ */
+public class LoadingDialog extends Dialog {
+    private ImageView iv;
+
+    public LoadingDialog(Context context) {
+        super(context);
+        /**设置对话框背景透明*/
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        setContentView(R.layout.loadingxml);
+        iv=(ImageView)findViewById(R.id.iv_loading);
+//        setCanceledOnTouchOutside(false);
+//        Animation animation=new ScaleAnimation(1.1f, 0.9f, 1.1f, 0.9f,
+//                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//        animation.setDuration(300);
+//        animation.setRepeatCount(300000000);
+//        iv.setAnimation(animation);
+////        iv.clearAnimation();
+//        iv.startAnimation(animation);
+        AnimationDrawable animationDrawable= (AnimationDrawable) iv.getDrawable();
+        animationDrawable.start();
+
+    }
+}
